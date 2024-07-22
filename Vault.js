ce925d59b05771c1c409ce28b39cde5b3c025dcc
Margo <giddens.margo@gmail.com>
2024-07-22 12:27:59 -0500

    add semicolons

--- Vault.js
+++ Vault.js
@@ -6,9 +6,9 @@
 5. User should see alert message saying: "You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39"
 */
 
-const num1 = 5 + 5 // num1 = 10
-const num2 = 80 / 2 // num2 = 40
-const num3 = 13 * 3 // num3 = 39
+const num1 = 5 + 5; // num1 = 10
+const num2 = 80 / 2; // num2 = 40
+const num3 = 13 * 3; // num3 = 39
 
 alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}`); // concatenated using backticks and ${} 
 
