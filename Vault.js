34ddb820babbe9f7d8a1aa36cd13675a44cd1e16
Margo <giddens.margo@gmail.com>
2024-07-22 12:15:34 -0500

    add variables and user alert

---
+++ Vault.js
@@ -0,0 +1,14 @@
+/* 
+1. Vault code is 10 - 40 - 39. 
+2. Establish each number as individual variables using unique equations.
+3. Deliver message to user via alert.
+4. Concatenate user message string to vault code variables in order.
+5. User should see alert message saying: "You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39"
+*/
+
+const num1 = 5 + 5 // num1 = 10
+const num2 = 80 / 2 // num2 = 40
+const num3 = 13 * 3 // num3 = 39
+
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}`); // concatenated using backticks and ${} 
+
